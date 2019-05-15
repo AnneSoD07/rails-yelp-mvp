94c5f91141bd93bbe73d4dcfd4b3d8c8bf29e0a2
anne sophie ducamin <annesophie.ducamin@gmail.com>
2019-05-15 17:16:50 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'destruction des restaurants'
+Restaurant.destroy_all
+puts 'tous les restaurants sont supprimés'
+puts 'creation de restaurants'
+Restaurant.create!({name: 'Les Sauvages', address: '33300 Bordeaux', category: 'french'})
+Restaurant.create!({name: 'Mona', address: '33000 Bordeaux', category: 'french'})
+Restaurant.create!({ name: 'Tripleta', address: '33300 Bordeaux', category: 'italian'})
+Restaurant.create!({name: 'Pepone', address: '33000 Bordeaux', category: 'italian'})
+Restaurant.create!({name: 'Lotus Bleu', address: '33200 Bordeaux', category: 'chinese'})
+puts 'c est créé!'
